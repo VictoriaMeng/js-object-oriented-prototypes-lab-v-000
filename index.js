ba94804856c1e3c585c69ebe1c68bd0562470492
Victoria Meng <nights1ststar@protonmail.com>
2018-08-16 18:15:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,5 +19,5 @@ function BoardMember(name, homeState, training){
   }
 }
 
-BoardMember.prototype.veto = function() {return "No, I must disagree"};
-BoardMember.prototype.approve = function() {return 'You can do that!'};
+BoardMember.prototype.veto = function () {return "No, I must disagree"};
+BoardMember.prototype.approve = function () {return 'You can do that!'};
