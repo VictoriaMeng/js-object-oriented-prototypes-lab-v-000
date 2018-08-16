4b8ae842c9d8eea17fd68dca585d78ad1c381605
Victoria Meng <nights1ststar@protonmail.com>
2018-08-16 18:17:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,10 +3,6 @@ function BoardMember(name, homeState, training){
   this.homeState = homeState
   this.training = training
 
-  this.releasePressStatement = function(){
-    return "You will see great things from Scuber."
-  }
-
   this.sayHi = function(){
     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
   }
@@ -15,3 +11,6 @@ function BoardMember(name, homeState, training){
 BoardMember.prototype.veto = function () {return "No, I must disagree"};
 BoardMember.prototype.approve = function () {return 'You can do that!'};
 BoardMember.prototype.doCharity = function () { return "I like to help people." };
+BoardMember.prototype.releasePressStatement = function(){
+  return "You will see great things from Scuber."
+};
